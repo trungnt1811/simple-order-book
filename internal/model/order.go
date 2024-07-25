aa34d609d170bcf1efaf04a4e2d018ebb950e444
trungnt1811 <trungqn.ptit@gmail.com>
2024-07-25 09:53:26 +0700

    refactor: add orderType

--- internal/model/order.go
+++ internal/model/order.go
@@ -1,11 +1,16 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"github.com/trungnt1811/simple-order-book/internal/constant"
+)
 
 type Order struct {
 	ID         uint64
 	CustomerID uint
 	Price      uint
 	Timestamp  time.Time
+	OrderType  constant.OrderType
 	GTT        *time.Time // Good Til Time
 }
--- internal/module/orderbook.go
+++ internal/module/orderbook.go
@@ -56,6 +56,7 @@ func (ob *OrderBook) SubmitOrder(customerID uint, price uint, orderType constant
 		Price:      price,
 		Timestamp:  time.Now(),
 		GTT:        gtt,
+		OrderType:  orderType,
 	}
 
 	ob.NextOrderID++
